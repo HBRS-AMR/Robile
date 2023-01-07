708e83f312ae1901538fde2e05e5c4a78ecd0b17
KishanSawant <kishansawant96@gmail.com>
2023-01-07 13:30:59 +0100

    [robile_bringup] ported to ros2

--- robile_bringup/setup.py
+++ robile_bringup/setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup
+import setuptools
 import os
 from glob import glob
 
 package_name = 'robile_bringup'
 
-setup(
+setuptools.setup(
     name=package_name,
     version='0.0.0',
     packages=[package_name],
@@ -12,9 +12,9 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name), glob('launch/*.xml'))
+        (os.path.join('share', package_name), glob('launch/*.py'))
     ],
-    install_requires=['setuptools'],
+    install_requires=['setuptools>=41.0'],
     zip_safe=True,
     maintainer='kelo',
     maintainer_email='kishansawant96@gmail.com',
